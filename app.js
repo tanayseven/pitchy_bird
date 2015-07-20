e627b294f9f64d546a4babb9d832872cab926099
Tanay PrabhuDesai <tanayseven@gmail.com>
2015-07-21 04:30:54 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,110 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2015 Tanay PrabhuDesai
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var https = require('https');
+var querystring = require('querystring');
+
+var env = (function(){
+      var Habitat = require("habitat");
+      Habitat.load();
+      return new Habitat();
+    }());
+
+var app = express();
+
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({ 
+  extended: true
+}));
+
+app.set('views',__dirname+'/views/');
+app.set('view engine','jade');
+app.engine('jade', require('jade').__express);
+
+// Use all the public files
+app.use(express.static(__dirname + '/public/js'));
+app.use(express.static(__dirname + '/public/css'));
+app.use(express.static(__dirname + '/public/css/font/'));
+app.use(express.static(__dirname + '/public/res'));
+
+function postCaptcha(response,ip,res_in,captchaVerified) {
+	var post_data = querystring.stringify({
+      'secret' : process.env.SECRET_KEY || env.get("SECRET_KEY"),
+      'response': response,
+      'remoteip': ip
+  });
+  console.log('Response:'+response);
+  console.log('Ip:'+ip);
+  var post_options = {
+      host: 'www.google.com',
+      port: '443',
+      path: '/recaptcha/api/siteverify',
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Length': post_data.length
+      }
+  };
+  var post_req = https.request(post_options, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+          console.log('Response: ' + chunk);
+          captchaVerified(chunk,res_in);
+      });
+  });
+  console.log('Post completed');
+  post_req.write(post_data);
+  post_req.end();
+}
+
+app.get('/', function (req, res) {
+  res.render('login.jade',{msg:''});
+});
+
+function captchaVerified(chunk, res) {
+	var txt = 'Please complete the captcha challenge';
+	var obj = JSON.parse(chunk);
+	console.log(typeof(obj)+' '+obj+' '+obj.success);
+	if (obj["success"]) {
+		res.render('game.jade');
+	} else {
+		res.render('login.jade',{msg:txt});
+	}
+}
+
+app.post('/play',function(req, res){
+	if (req.body['username'] == '') {
+		var txt = 'Username cannot be left empty';
+  	res.render('login.jade',{msg:txt});
+	} else {
+		postCaptcha(req.body['g-recaptcha-response'],req.ip,res,captchaVerified)
+	}
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Pitchy Bird started at http://%s:%s', host, port);
+});
---
+++ public/js/game.js
@@ -0,0 +1,239 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2015 Tanay PrabhuDesai
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+function loaded () {
+  //Add stuff here
+  var audioReady = false;
+  var audioInput = {
+    analyser:null,
+    waveArray:null,
+    freqArray:null,
+    freq:{min:8,max:40},
+    ampl:{}
+  };
+  audioInput.init = function() {
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    var audioContext = new AudioContext();
+    var mediaStreamSource = null;
+    var gotStream = null;
+    var error = null;
+    var dictionary = {
+      "audio": {
+        "mandatory": {
+          "googEchoCancellation": "true",
+          "googAutoGainControl": "false",
+          "googNoiseSuppression": "true",
+          "googHighpassFilter": "true"
+        },
+        "optional": []
+      },
+    };
+    gotStream = function (stream) {
+      console.log(this);
+      mediaStreamSource = audioContext.createMediaStreamSource(stream);
+      audioInput.analyser = audioContext.createAnalyser();
+      audioInput.analyser.fftSize = 2048;
+      bufferLength = audioInput.analyser.fftSize;
+      audioInput.waveArray = new Uint8Array(bufferLength);
+      audioInput.freqArray = new Uint8Array(bufferLength);
+      mediaStreamSource.connect( audioInput.analyser );
+      audioInput.analyser.getByteTimeDomainData(audioInput.waveArray);
+      audioInput.analyser.getByteFrequencyData(audioInput.freqArray);
+      console.log(audioInput.freqArray);
+      console.log("Created audio stream");
+      audioReady = true;
+    };
+    error = function() {
+      alert('Stream generation failed.');
+    };
+    try {
+      navigator.getUserMedia = 
+        navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia;
+      navigator.getUserMedia(dictionary, gotStream, error);
+    } catch (e) {
+      alert('Could not detect the mic' + e);
+    }
+  };
+  audioInput.getMaxFreq = function() {
+    audioInput.analyser.getByteFrequencyData(audioInput.freqArray);
+    var arr = audioInput.freqArray, max = 0;
+    for (var i = 0 ; i < arr.length ; ++i ) {
+      if (arr[i] > arr[max]) max = i;
+    }
+    return max;
+  };
+  audioInput.getMaxAmpl = function() {
+    audioInput.analyser.getByteTimeDomainData(audioInput.waveArray);
+    var arr = audioInput.waveArray, max = 0;
+    for (var i = 0 ; i < arr.length ; ++i ) {
+      if (arr[i] > max) max = arr[i];
+    }
+    return max;
+  };
+  var player = {
+    x:0,
+    y:0,
+    width:20,
+    height:20,
+    downSpeed:0
+  };
+  player.init = function () {
+    player.x = ( global.width + player.width ) / 2;
+  }
+  player.testCollision = function() {
+    var res = false;
+    var i = 0;
+    while ( player.x > obstacles.onScreen[i].x + obstacles.width ) ++i;
+    global.new_obs = i;
+    if (global.old_obs != global.new_obs) {
+      ++global.score;
+      global.old_obs = global.new_obs;
+    }
+    var obs = obstacles.onScreen[i];
+    if ( ! ( player.x + player.width < obs.x || player.x > obs.x + obstacles.width ) // when not in the obstacle
+      && (player.y < obs.y - obstacles.gap/2 || player.y + player.width > obs.y + obstacles.gap/2) // when in the gap
+      )
+      res = true;
+    return res;
+  };
+  player.update = function () {
+    if(player.testCollision()) {
+      window.alert("Game Over");
+    }
+    if ( audioInput.getMaxAmpl() > 140 ) {
+      player.downSpeed = 0;//stop current motion direction and move upwards
+      player.downSpeed -= (audioInput.getMaxFreq() - audioInput.freq.min)/2;//dividing factor(convert to a constant later)
+    }
+    else {
+      if (player.downSpeed < global.gravity.t )
+        player.downSpeed += global.gravity.f;// apply gravity
+    }
+    player.y += player.downSpeed;//move the player WRT its speed
+    if ( player.y + player.height < 0) {
+      player.y = 0;//if player tries to go out of the screen from top
+      player.downSpeed = 0;
+    }
+    else if ( player.y >= global.height - player.height ) {
+      player.y = global.height - player.height;//same thing but from bottom
+      player.downSpeed = 0;
+    }
+  };
+  player.draw = function (ctx) {
+    ctx.fillRect(player.x,player.y,player.width,player.height);
+  };
+  var global = {
+    width:0,
+    height:0,
+    score:0,
+    old_obs:0,
+    new_obs:0,
+    gravity: {t:7,f:0.5}
+  };
+  var obstacles = {
+    list:[],
+    next:0,
+    onScreen:[],
+    gap:100,
+    width:50,
+    speed:5
+  };
+  var generateObstacles = function(n) {
+    var levels = 20;
+    var lst = [];
+    for ( var i  = 0 ;  i < n ; ++i ) {
+      lst.push(Math.random());
+    }
+    return lst;
+  };
+  obstacles.addObstacle = function() {
+    while (obstacles.onScreen.length < 6) {
+      var obj = {};
+      obj.y = obstacles.list[obstacles.next] * global.height;
+      if (obstacles.onScreen.length <= 0)
+        obj.x = global.width;
+      else
+        obj.x = obstacles.onScreen[obstacles.onScreen.length-1].x + ( global.width / 5 );
+      obstacles.onScreen.push(obj);
+      obstacles.next++;
+    }
+  };
+  obstacles.init = function () {
+    obstacles.addObstacle();
+  };
+  obstacles.update = function () {
+    while (obstacles.onScreen.length < 6) {
+      obstacles.addObstacle();
+    }
+    for ( var i = 0 ; i < obstacles.onScreen.length ; ++i ) {
+      --obstacles.onScreen[i].x;
+    }
+    if (obstacles.onScreen[0].x + obstacles.width < 0) {
+      obstacles.onScreen.splice(0,1);
+    }
+//    console.log(obstacles.onScreen);
+  };
+  obstacles.draw = function (ctx) {
+    for ( var i = 0 ; i < obstacles.onScreen.length ; ++i ) {
+      ctx.fillRect(obstacles.onScreen[i].x,0,obstacles.width,obstacles.onScreen[i].y-obstacles.gap/2);
+      ctx.fillRect(obstacles.onScreen[i].x,obstacles.onScreen[i].y+obstacles.gap/2,obstacles.width,global.height);
+    }
+  };
+  function init() {
+    var canvas = document.getElementById('game_canvas');
+    global.ctx = canvas.getContext("2d");
+    obstacles.list = generateObstacles(10000);
+    global.width = canvas.width  = window.innerWidth;
+    global.height = canvas.height = window.innerHeight;
+    audioInput.init();
+    player.init();
+    obstacles.init();
+  }
+  function update() {
+    player.update();
+    obstacles.update();
+  }
+  function draw() {
+    ctx = global.ctx;
+    ctx.fillStyle = "black";
+    ctx.font="30px Press Start K";
+    ctx.clearRect(0,0,global.width,global.height);
+    ctx.fillStyle = "white";
+    player.draw(ctx);
+    obstacles.draw(ctx);
+    ctx.fillStyle = "black";
+    ctx.fillRect(0,0,300,60);
+    ctx.fillStyle = "white";
+    ctx.fillText("Score: "+global.score,10,50);
+  }
+  init();
+  var step = function(timestamp) {
+    if ( audioReady ) {
+      update();
+      draw();
+    }
+    window.requestAnimationFrame(step);
+  };
+  window.requestAnimationFrame(step);
+};
+window.onload = loaded;
