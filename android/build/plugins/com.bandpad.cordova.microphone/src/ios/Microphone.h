5d047d0f2436ee333966d140ef9ed226777418dc
Tanay PrabhuDesai <tanayseven@gmail.com>
2017-09-04 00:50:51 +0530

    Delete build files and add build directory to gitignroe

--- android/build/plugins/com.bandpad.cordova.microphone/src/ios/Microphone.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <AVFoundation/AVFoundation.h>
-#import <Cordova/CDVPlugin.h>
-
-@interface Microphone : CDVPlugin
-
-- (void)microphone:(CDVInvokedUrlCommand*)command;
-
-@end
--- android/build/plugins/com.bandpad.cordova.microphone/www/ios/microphone.js
+++
@@ -1,8 +0,0 @@
-var
-  exec = require('cordova/exec');
-
-module.exports = function (next) {
-  exec(function (granted) {
-    next(granted);
-  }, null, 'Microphone', 'microphone');
-};
